dce4c311a41821425e88cebc907310d1b2b2d38d
krc <krc@netgrif.com>
2020-12-20 00:41:58 +0100

    BasicCombat
    - Add deal damage function
    - Add health class

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -6,16 +6,37 @@ using Outcast.Movement;
 namespace Outcast.Combat {
     public class Fighter : MonoBehaviour, IAction {
         [SerializeField] private float weaponRange = 2f;
+        [SerializeField] private float weaponDamage = 5f;
+        [SerializeField] private float timeBetweenAttack = 0.7f;
         private Transform target;
 
+        private float timeSinceLastAttack = 0f;
+
         private void Update() {
+            timeSinceLastAttack += Time.deltaTime;
+            if (timeSinceLastAttack < 0) timeSinceLastAttack = 0f;
             if (target == null) return;
             if (!GetIsInRange()) {
+                timeSinceLastAttack = 0f;
                 GetComponent<Mover>().MoveTo(target.position);
             }
             else {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour() {
+            if (timeBetweenAttack < timeSinceLastAttack) {
+                // This call hit event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0f;
+            }
+        }
+        
+        // Hit event called from animation
+        void Hit() {
+            target.GetComponent<Health>().TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange() {
---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Outcast.Combat {
+    public class Health : MonoBehaviour {
+        [SerializeField] private float health;
+
+        public void TakeDamage(float demage) {
+            health = Mathf.Max(health - demage, 0);
+            print(health);
+        }
+    }
+}
