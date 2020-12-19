16278a40b543126749c3ea96ed56312134328e00
krc <krc@netgrif.com>
2020-12-19 14:57:39 +0100

    First commit
    - Initial movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+    private Ray lastRay;
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
