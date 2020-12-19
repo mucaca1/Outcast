342afc0a9cf067ea91c59723d69368910850aff7
krc <krc@netgrif.com>
2020-12-19 16:25:31 +0100

    BasicMovement
    - Change enviroment

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+    void Update() {
+        transform.position = target.position;
+    }
+}
