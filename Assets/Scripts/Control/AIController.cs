a68af168de5479fc3beea4f175506fe643010a8b
krc <krc@netgrif.com>
2020-12-20 15:36:29 +0100

    EnemyAI
    - Add Patrol behaviour

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -8,6 +8,9 @@ using UnityEngine.AI;
 namespace Outcast.Control {
     public class AIController : MonoBehaviour {
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspicionTime = 3f;
+        [SerializeField] private PatrolPath patrolPath;
+        [SerializeField] private float waypointTolerance = 1f;
 
         private GameObject _player;
         private Fighter _fighter;
@@ -15,6 +18,9 @@ namespace Outcast.Control {
         private Health _health;
 
         private Vector3 _guardPosition;
+        private float _lastSinceLastSawPlayer = Mathf.Infinity;
+
+        private int _currentWaypointIndex = 0;
 
         private void Start() {
             _player = GameObject.FindWithTag("Player");
@@ -25,15 +31,54 @@ namespace Outcast.Control {
         }
 
         private void Update() {
-            
             if (_health.IsDead) return;
-            
+
             if (InAttackRangeOfThePlayer() && _fighter.CanAttack(_player)) {
-                _fighter.Attack(_player);
+                _lastSinceLastSawPlayer = 0f;
+                AttackBehaviour();
+            }
+            else if (_lastSinceLastSawPlayer < suspicionTime) {
+                SuspicionBehaviour();
             }
             else {
-                _mover.StartMoveAction(_guardPosition);
+                PatrolBehaviour();
             }
+
+            _lastSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour() {
+            Vector3 nextPosition = _guardPosition;
+            if (patrolPath != null) {
+                if (AtWaypoint()) {
+                    CycleWaypoint();
+                }
+
+                nextPosition = GetCurrentWaypoint();
+            }
+
+            _mover.StartMoveAction(nextPosition);
+        }
+
+        private Vector3 GetCurrentWaypoint() {
+            return patrolPath.GetWaypointPosition(_currentWaypointIndex);
+        }
+
+        private void CycleWaypoint() {
+            _currentWaypointIndex = patrolPath.GetNextWayoint(_currentWaypointIndex);
+        }
+
+        private bool AtWaypoint() {
+            float distance = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distance < waypointTolerance;
+        }
+
+        private void SuspicionBehaviour() {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour() {
+            _fighter.Attack(_player);
         }
 
         private bool InAttackRangeOfThePlayer() {
