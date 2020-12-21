a281959f260ba54b4ba7f773ec73dc3f27be8868
krc <krc@netgrif.com>
2020-12-21 19:26:08 +0100

    SceneManagement
    - Add portals
    - Add Fader
    - Add second sandbox

---
+++ Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Outcast.Core {
+    public class PersistentObjectSpawner : MonoBehaviour {
+        [SerializeField] private GameObject permistentObjectPrefab;
+
+        private bool wasSpawned = false;
+        private void Awake() {
+            if (wasSpawned) return;
+
+            SpawnPersistentObject();
+
+            wasSpawned = true;
+        }
+
+        private void SpawnPersistentObject() {
+            GameObject persistentObject = Instantiate(permistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Outcast.Management {
+    public class Portal : MonoBehaviour {
+
+        enum DestinationIdentifier {
+            A, B, C, D, E
+        }
+
+        [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private DestinationIdentifier destination;
+        [SerializeField] private float fadeOutTime = 0.5f;
+        [SerializeField] private float fadeInTime = 1.5f;
+        [SerializeField] private float fadeWaitTime = 0.5f;
+        private void OnTriggerEnter(Collider other) {
+            if (other.tag == "Player") {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition() {
+            if (sceneToLoad < 0) {
+                Debug.LogError("Portal does not has select scene");
+                yield break;
+            }
+            DontDestroyOnLoad(gameObject);
+
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetAnotherPortal();
+            UpdatePlayer(otherPortal);
+            
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal) {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetAnotherPortal() {
+            foreach (Portal portal in FindObjectsOfType<Portal>()) {
+                if (portal == this) continue;
+                if (portal.destination == this.destination)
+                    return portal;
+            }
+
+            return null;
+        }
+    }
+}
