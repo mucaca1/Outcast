e0785200bece052c913326f5c18c1d238d741b8d
krc <krc@netgrif.com>
2020-12-23 17:51:29 +0100

    CharacterStats
    - Add basic stats for characters
    - Add enum of character types

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Outcast.Stats {
+    public class BaseStats : MonoBehaviour {
+        [Range(1, 99)]
+        [SerializeField] private int startingLevel = 1;
+        [SerializeField] private CharacterClass characterClass;
+    }
+}
