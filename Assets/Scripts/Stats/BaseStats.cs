19de1c2f428800728c0cf10c7618c94396f1fce3
krc <krc@netgrif.com>
2020-12-23 18:00:51 +0100

    CharacterStats
    - Add progression structure as scriptable object

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -6,5 +6,6 @@ namespace Outcast.Stats {
         [Range(1, 99)]
         [SerializeField] private int startingLevel = 1;
         [SerializeField] private CharacterClass characterClass;
+        [SerializeField] private Progression _progression = null;
     }
 }
---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Outcast.Stats {
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New progression", order = 0)]
+    public class Progression : ScriptableObject {
+
+        [SerializeField] private ProgressionCharacterClass[] _characterClass = null;
+
+        [System.Serializable]
+        class ProgressionCharacterClass {
+            [SerializeField] private CharacterClass characterClass;
+            [SerializeField] private float[] life;
+        }
+    }
+}
