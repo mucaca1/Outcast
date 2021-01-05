5385fa52416ce28e78527ec5eccb2799e75d8e7e
krc <krc@netgrif.com>
2021-01-05 17:54:41 +0100

    DialogueGameplay
    - Add showing next content

--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
 namespace Dialogue {
     public class PlayerConversant : MonoBehaviour {
         [SerializeField] private Dialogue currentDialogue;
+        private DialogueNode _currentNode = null;
+
+        private void Awake() {
+            _currentNode = currentDialogue.GetRootNode();
+        }
 
         public string GetText() {
-            if (currentDialogue == null) {
+            if (_currentNode == null) {
                 return "";
             }
 
-            return currentDialogue.GetRootNode().GetText();
+            return _currentNode.GetText();
+        }
+
+        public void Next() {
+            _currentNode = currentDialogue.GetAllChildren(_currentNode).ToArray()[0];
+        }
+
+        public bool HasNext() {
+            return currentDialogue.GetAllChildren(_currentNode).Count() > 0;
         }
     }
 }
--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -2,16 +2,29 @@
 using Dialogue;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Outcast.UI {
     public class DialogueUI : MonoBehaviour {
         private PlayerConversant _playerConversant;
 
         [SerializeField] private TextMeshProUGUI AIText;
+        [SerializeField] private Button nextButton;
 
         private void Start() {
             _playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            nextButton.onClick.AddListener(Next);
+            UpdateUI();
+        }
+
+        private void Next() {
+            _playerConversant.Next();
+            UpdateUI();
+        }
+
+        private void UpdateUI() {
             AIText.text = _playerConversant.GetText();
+            nextButton.gameObject.SetActive(_playerConversant.HasNext());
         }
     }
 }
