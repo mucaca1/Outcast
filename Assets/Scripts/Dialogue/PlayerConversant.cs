7fe4dc1e4e49571e9c2208522360d122c6d26c6c
krc <krc@netgrif.com>
2021-01-05 18:43:17 +0100

    DialogueGameplay
    - Add selecting choices

--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Dialogue {
     public class PlayerConversant : MonoBehaviour {
         [SerializeField] private Dialogue currentDialogue;
         private DialogueNode _currentNode = null;
+
+        private bool _isChoosing = false;
 
         private void Awake() {
             _currentNode = currentDialogue.GetRootNode();
@@ -19,8 +23,30 @@ namespace Dialogue {
             return _currentNode.GetText();
         }
 
+        public void SelectChoice(DialogueNode chosenNode) {
+            _currentNode = chosenNode;
+            _isChoosing = false;
+            Next();
+        }
+
+        public bool IsChoosing() {
+            return _isChoosing;
+        }
+
         public void Next() {
-            _currentNode = currentDialogue.GetAllChildren(_currentNode).ToArray()[0];
+            int numPlayerResponsies = currentDialogue.GetPlayerChildren(_currentNode).Count();
+            if (numPlayerResponsies > 0) {
+                _isChoosing = true;
+                return;
+            }
+
+            DialogueNode[] nodes = currentDialogue.GetAIChildren(_currentNode).ToArray();
+            int randomIndex = Random.Range(0, nodes.Length);
+            _currentNode = nodes[randomIndex];
+        }
+
+        public IEnumerable<DialogueNode> GetChoices() {
+            return currentDialogue.GetPlayerChildren(_currentNode);
         }
 
         public bool HasNext() {
--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -10,6 +10,9 @@ namespace Outcast.UI {
 
         [SerializeField] private TextMeshProUGUI AIText;
         [SerializeField] private Button nextButton;
+        [SerializeField] private Transform choiceRoot;
+        [SerializeField] private GameObject choicePrefab;
+        [SerializeField] private GameObject AIResponse;
 
         private void Start() {
             _playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
@@ -23,8 +26,33 @@ namespace Outcast.UI {
         }
 
         private void UpdateUI() {
-            AIText.text = _playerConversant.GetText();
-            nextButton.gameObject.SetActive(_playerConversant.HasNext());
+            AIResponse.SetActive(!_playerConversant.IsChoosing());
+            choiceRoot.gameObject.SetActive(_playerConversant.IsChoosing());
+
+            if (_playerConversant.IsChoosing()) {
+                BuildChoiceList();
+            }
+            else {
+                AIText.text = _playerConversant.GetText();
+                nextButton.gameObject.SetActive(_playerConversant.HasNext());
+            }
+
+        }
+
+        private void BuildChoiceList() {
+            foreach (Transform child in choiceRoot) {
+                Destroy(child.gameObject);
+            }
+
+            foreach (DialogueNode choice in _playerConversant.GetChoices()) {
+                GameObject choiceButton = Instantiate(choicePrefab, choiceRoot);
+                choiceButton.GetComponentInChildren<TextMeshProUGUI>().text = choice.GetText();
+                Button button = choiceButton.GetComponentInChildren<Button>();
+                button.onClick.AddListener(() => {
+                    _playerConversant.SelectChoice(choice);
+                    UpdateUI();
+                });
+            }
         }
     }
 }
