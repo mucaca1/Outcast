1c1dd51b7ed92fd64c6149dda20b0b8c1de07e90
krc <krc@netgrif.com>
2021-01-05 12:02:52 +0100

    DialogueGameplay
    - Add player conversant for showing data to UI

---
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Dialogue {
+    public class PlayerConversant : MonoBehaviour {
+        [SerializeField] private Dialogue currentDialogue;
+
+        public string GetText() {
+            if (currentDialogue == null) {
+                return "";
+            }
+
+            return currentDialogue.GetRootNode().GetText();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dialogue;
+using TMPro;
+using UnityEngine;
+
+namespace Outcast.UI {
+    public class DialogueUI : MonoBehaviour {
+        private PlayerConversant _playerConversant;
+
+        [SerializeField] private TextMeshProUGUI AIText;
+
+        private void Start() {
+            _playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.text = _playerConversant.GetText();
+        }
+    }
+}
