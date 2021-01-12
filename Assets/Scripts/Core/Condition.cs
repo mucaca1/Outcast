0b589f39678208ae100bb9f9ed7776bf1afefaac
krc <krc@netgrif.com>
2021-01-12 10:04:41 +0100

    DialogueGameplay
    - Add sub conditions

--- Assets/Scripts/Core/Condition.cs
+++ Assets/Scripts/Core/Condition.cs
@@ -3,21 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Outcast.Core {
-    
     [System.Serializable]
     public class Condition {
-        [SerializeField] private string predicate;
-        [SerializeField] private string[] parameters;
-
+        [SerializeField] private Disjunction[] and;
+        
         public bool Check(IEnumerable<IPredicateEvaluator> evaluators) {
-            foreach (var evaluator in evaluators) {
-                bool? result = evaluator.Evaluate(predicate, parameters);
-                if (result == null) continue;
-
-                if (result == false) return false;
+            foreach (var dis in and) {
+                if (!dis.Check(evaluators)) {
+                    return false;
+                }
             }
 
             return true;
         }
+        
+        [System.Serializable]
+        class Disjunction {
+            [SerializeField] private Predicate[] or;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators) {
+                foreach (var pred in or) {
+                    if (pred.Check(evaluators)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+            
+        [System.Serializable]
+        public class Predicate {
+            [SerializeField] private string predicate;
+            [SerializeField] private string[] parameters;
+            [SerializeField] private bool negate = false;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators) {
+                foreach (var evaluator in evaluators) {
+                    bool? result = evaluator.Evaluate(predicate, parameters);
+                    if (result == null) continue;
+
+                    if (result == negate) return false;
+                }
+
+                return true;
+            }
+        }
     }
+
 }
